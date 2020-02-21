0380a16f21b2f64bf554016820ffd670714f579c
Juhi <shahjuhi1108@gmail.com>
2020-02-20 21:09:45 -0800

    Address review comments

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,6 +11,15 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String KEY_NAME = "name";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) {
 
         Sandwich sandwich = null;
@@ -20,19 +29,19 @@ public class JsonUtils {
 
             sandwich = new Sandwich();
 
-            JSONObject name = jsonObject.getJSONObject("name");
+            JSONObject name = jsonObject.getJSONObject(KEY_NAME);
 
-            sandwich.setMainName(name.getString("mainName"));
+            sandwich.setMainName(name.getString(KEY_MAIN_NAME));
 
-            sandwich.setAlsoKnownAs(parseJsonArray(name.getJSONArray("alsoKnownAs")));
+            sandwich.setAlsoKnownAs(parseJsonArray(name.getJSONArray(KEY_ALSO_KNOW_AS)));
 
-            sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
+            sandwich.setPlaceOfOrigin(jsonObject.getString(KEY_PLACE_OF_ORIGIN));
 
-            sandwich.setDescription(jsonObject.getString("description"));
+            sandwich.setDescription(jsonObject.getString(KEY_DESCRIPTION));
 
-            sandwich.setImage(jsonObject.getString("image"));
+            sandwich.setImage(jsonObject.getString(KEY_IMAGE));
 
-            sandwich.setIngredients(parseJsonArray(jsonObject.getJSONArray("ingredients")));
+            sandwich.setIngredients(parseJsonArray(jsonObject.getJSONArray(KEY_INGREDIENTS)));
 
         } catch (JSONException e) {
             e.printStackTrace();
